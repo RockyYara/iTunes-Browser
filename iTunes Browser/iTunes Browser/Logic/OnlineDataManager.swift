677bf04f79a73a09968f86d152c6756c2e2fa4b3
Yaroslav Sverdlikov <rockyyara@gmail.com>
2019-05-30 15:18:18 +0300

    Implements tracking download statuses for images. Prevents multiple downloads of the same image (earlier this could happen on slow networks if user scrolled table view in opposite directions several times). Optimizes network load and animations for image showing and updating.

--- iTunes Browser/iTunes Browser/Logic/OnlineDataManager.swift
+++ iTunes Browser/iTunes Browser/Logic/OnlineDataManager.swift
@@ -40,23 +40,49 @@ class OnlineDataManager {
     }
     
     func downloadImage(for item: Item, completionHandler: @escaping (Bool) -> Void) {
-        guard let url = item.artworkUrl60 else {
-            completionHandler(false)
-            return
-        }
-        
-        apiHelper.externalResourceGetData(with: url) { resultData in
-            guard let data = resultData else {
+        switch item.imageDownloadStatus {
+        case .notStarted, .failed:
+            guard let url = item.artworkUrl60 else {
+                // If we don't have image url it's not possible to download the image for given item.
+                item.imageDownloadStatus = .notPossible
                 completionHandler(false)
                 return
             }
             
-            if let image = UIImage(data: data) {
-                item.image = image
-                completionHandler(true)
-            } else {
-                completionHandler(false)
+            item.imageDownloadStatus = .inProgress
+            
+            self.apiHelper.externalResourceGetData(with: url) { resultData in
+                guard let data = resultData else {
+                    item.imageDownloadStatus = .failed
+                    completionHandler(false)
+                    return
+                }
+                
+                if let image = UIImage(data: data) {
+                    item.image = image
+                    item.imageDownloadStatus = .complete
+                    // Order of these 2 lines is very important for thread safety, because they are going to be executed on background thread,
+                    // and at the same time check if item.image is set can be run on the main thread (in method
+                    // configure(_ cell: ItemTableViewCell, for item: Item, at indexPath: IndexPath) of OnlineItemsViewController class).
+                    
+                    completionHandler(true)
+                } else {
+                    item.imageDownloadStatus = .failed
+                    completionHandler(false)
+                }
             }
+        case .notPossible:
+            completionHandler(false)
+        case .inProgress:
+            // If download is already in progress, we shouldn't run completion handler at all,
+            // because download status "in progress" means that completion closure for given item already exists
+            // and will be run after download completes or fails.
+            break
+        case .complete:
+            // In general we shouldn't get in this case, because each class which uses item and needs its image should check if item.image is already set,
+            // and only try to initiate download if it isn't.
+            // But if a class doesn't do this check on its own, we should execute completion closure.
+            completionHandler(true)
         }
     }
 }
--- iTunes Browser/iTunes Browser/Model/Item.swift
+++ iTunes Browser/iTunes Browser/Model/Item.swift
@@ -19,6 +19,8 @@ class Item {
     let artworkUrl60: String?
     
     var image: UIImage?
+    var imageDownloadStatus = DownloadStatus.notStarted
+    // At the beginning every item will start without downloaded image. It will be downloaded on demand.
     
     init(type: ItemType, trackId: Int, trackName: String, artistName: String, artworkUrl60: String?) {
         self.type = type
@@ -26,7 +28,6 @@ class Item {
         self.trackName = trackName
         self.artistName = artistName
         self.artworkUrl60 = artworkUrl60
-        self.image = nil
     }
     
 }
