b533a84afb5011fe3826a455f6d4d72b16e7b873
Yaroslav Sverdlikov <rockyyara@gmail.com>
2019-06-11 19:31:31 +0300

    Adds ItemTableViewCell to the umbrella header of the framework.

--- ITunesBrowsing/ITunesBrowsing/ITunesBrowsing.h
+++ ITunesBrowsing/ITunesBrowsing/ITunesBrowsing.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ITunesBrowsingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ITunesBrowsing/PublicHeader.h>
 
-
+#import "ItemTableViewCell.h"
