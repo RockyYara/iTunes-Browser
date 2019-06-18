bd953abfa455dae41dc290bd31b59ec844f2e99a
Yaroslav Sverdlikov <rockyyara@gmail.com>
2019-06-18 20:33:31 +0300

    Makes some more framework entities public.

--- ITunesBrowsing/ITunesBrowsing/Model/Item.swift
+++ ITunesBrowsing/ITunesBrowsing/Model/Item.swift
@@ -10,19 +10,19 @@ import Foundation
 
 public class Item: NSObject {
     
-    let type: ItemType
+    public let type: ItemType
     
-    let trackId: Int
-    let trackName: String
-    let artistName: String
+    public let trackId: Int
+    public let trackName: String
+    public let artistName: String
     
-    let artworkUrl60: String?
+    public let artworkUrl60: String?
     
-    var image: UIImage?
-    var imageDownloadStatus = DownloadStatus.notStarted
+    public var image: UIImage?
+    public var imageDownloadStatus = DownloadStatus.notStarted
     // At the beginning every item will start without downloaded image. It will be downloaded on demand.
     
-    init(type: ItemType, trackId: Int, trackName: String, artistName: String, artworkUrl60: String?) {
+    public init(type: ItemType, trackId: Int, trackName: String, artistName: String, artworkUrl60: String?) {
         self.type = type
         self.trackId = trackId
         self.trackName = trackName
